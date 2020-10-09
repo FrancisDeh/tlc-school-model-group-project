0ad28fcba899f2e17db0cf50476414f1752b8b52
ralphmensah <ralphmensah123@gmail.com>
2020-10-09 14:29:16 +0000

    Updated NaughtyStudentTest

--- test/com/company/project/NaughtyStudentTest.java
+++ test/com/company/project/NaughtyStudentTest.java
@@ -19,5 +19,17 @@ class NaughtyStudentTest {
         student3.setGrade(85.0);
         assertEquals(85.25,student3.getAverageGrade(),"naughty student average grade is not 10 percent higher!");
     }
+    @Test
+    public void naughtyStudentsScoresHigher(){
+        Student student3 = new NaughtyStudent();
+        student3.setGrade(70.0);
+        student3.setGrade(85.0);
+
+        Student student2 = new Student();
+        student2.setGrade(70.0);
+        student2.setGrade(85.0);
+
+        assertTrue(student3.getAverageGrade() > student2.getAverageGrade(),"naughty student did not score higher than other students");
+    }
 }
 
