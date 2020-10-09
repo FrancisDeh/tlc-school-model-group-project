6ced7055a1435fe6d747897527e62df77359ec74
ralphmensah <ralphmensah123@gmail.com>
2020-10-09 12:41:10 +0000

    Implemented NaughtyStudentTest

--- test/com/company/project/NaughtyStudentTest.java
+++ test/com/company/project/NaughtyStudentTest.java
@@ -9,9 +9,15 @@ class NaughtyStudentTest {
     @Test
     public void naughtyStudentIsAKindOfStudent(){
         Student naughtyStudent = new NaughtyStudent();
-
-
+        assertTrue(naughtyStudent instanceof Student,"naughtyStudent is not a kind of Student");
 
     }
-
+    @Test
+    public void naughtyStudentAverageGradeIsHigher(){
+        Student student3 = new NaughtyStudent();
+        student3.setGrade(70.0);
+        student3.setGrade(85.0);
+        assertEquals(85.25,student3.getAverageGrade(),"naughty student average grade is not 10 percent higher!");
+    }
 }
+
