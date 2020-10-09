6f566f3b754bb8e1bd5bbf4f9dd88f266fb7c0a7
Francis Deh <francis.deh@turntabl.io>
2020-10-09 18:53:22 +0000

    Refactored Register class with streams

--- src/com/company/project/Register.java
+++ src/com/company/project/Register.java
@@ -2,37 +2,44 @@ package com.company.project;
 
 import javax.lang.model.type.NullType;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Register {
-    private List<Student> nameables = new ArrayList<>();
+    private List<? extends Student> nameables = new ArrayList<>();
 
-    public Register(List<Student> students){
+    public Register(List<? extends Student> students){
         this.nameables = students;
     }
 
     public List<String> getRegister(){
-        List<String> names = new ArrayList<>();
-        for (Nameable nameable: this.nameables) {
-            names.add(nameable.getName());
-        }
-        return names;
+//        List<String> names = new ArrayList<>();
+//        for (Nameable nameable: this.nameables) {
+//            names.add(nameable.getName());
+//        }
+//        return names;
+
+       return this.nameables.stream()
+               .map(Student::getName)
+               .collect(Collectors.toList());
     }
 
     public Map<Level, List<Student>> getRegisterByLevel(Level level) {
         List<Student> students = new ArrayList<>();
 
-        for (Student student: this.nameables) {
-            if(student.getLevel().equals(level)) {
-                students.add(student);
-            }
-        }
+//        for (Student student: this.nameables) {
+//            if(student.getLevel().equals(level)) {
+//                students.add(student);
+//            }
+//        }
+        students = this.nameables.stream()
+                .filter(student -> student.getLevel().equals(level))
+                .collect(Collectors.toList());
 
         //{
         // ONE: [Student1, Student2..]
         // }
         Map<Level, List<Student>> studentMap = new HashMap<>();
         studentMap.put(level, students);
-
         return studentMap;
     }
 
@@ -50,39 +57,43 @@ public class Register {
         // -- xyz
         StringBuilder builder = new StringBuilder();
         builder.append("Level One\n");
-        for (Student student: levelOneStudents) {
-            builder.append(student.getName().concat("\n"));
-        }
-
+//        for (Student student: levelOneStudents) {
+//            builder.append(student.getName().concat("\n"));
+//        }
+        levelOneStudents.forEach(student -> builder.append(student.getName().concat("\n")));
         builder.append("\nLevel Two\n");
-        for (Student student: levelTwoStudents) {
-            builder.append(student.getName().concat("\n"));
-        }
-
+//        for (Student student: levelTwoStudents) {
+//            builder.append(student.getName().concat("\n"));
+//        }
+        levelTwoStudents.forEach(student -> builder.append(student.getName().concat("\n")));
         return builder.toString();
     }
 
-    public List<Student> sort(Comparator<Student> comparator) {
+    public List<? extends Student> sort(Comparator<Student> comparator) {
         Collections.sort(this.nameables, comparator);
         return this.nameables;
     }
 
     public Student getStudentByName(String name) throws StudentNotFoundException  {
-        Student student = new Student();
-        boolean studentIsFound = false;
+//        Student student = new Student();
+//        boolean studentIsFound = false;
 
-        for (Student s: this.nameables) {
-            if(s.getName().equals(name)) {
-                student = s;
-                studentIsFound = true;
-                break;
-            }
-        }
+//        for (Student s: this.nameables) {
+//            if(s.getName().equals(name)) {
+//                student = s;
+//                studentIsFound = true;
+//                break;
+//            }
+//        }
 
-        if(!studentIsFound) {
+       List<Student> foundStudents = this.nameables.stream().
+               filter(s -> s.getName().equals(name))
+               .collect(Collectors.toList());
+
+        if(foundStudents.isEmpty()) {
             throw new StudentNotFoundException("Student ".concat(name).concat(" not found"));
         }
 
-        return student;
+        return foundStudents.get(0);
     }
 }
